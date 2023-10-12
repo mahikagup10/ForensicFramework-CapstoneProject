cc6455c433c330603c049b7024c2ed07d861c4be
Mahesh KM <mkm63806@gmail.com>
2023-10-12 10:07:07 +0530

    Add files via upload
    Updated ils and istat

--- istat.c
+++ istat.c
@@ -3,38 +3,43 @@
 FILE* file;
 char path[100];
 
-int ils(int offset) {
-	FILE* fpipe;
+int istat(int offset, int inode) {
+    	FILE* fpipe;
 	char c = 0;
 	char command[256];
 	file = fopen("file_address.txt","r");
 	if(fgets(path,sizeof(path),file) != NULL){
-		snprintf(command, sizeof(command), "ils -f ntfs -o %d %s", offset, path);
-		fpipe = (FILE*)popen(command, "r");	    
-		if(fpipe == NULL){
-		perror("Error opening pipe");
-		exit(EXIT_FAILURE);
+		snprintf(command, sizeof(command), "istat -o %d %s %d", offset, path, inode);
+		
+		fpipe = (FILE*)popen(command, "r");
+		
+		if(fpipe==NULL){
+			perror("Error opening pipe");
+			exit(EXIT_FAILURE);
 		}
+
 		while(fread(&c, sizeof c, 1, fpipe)){
-		printf("%c", c);
-		}    
+			printf("%c", c);
+		}
+		
 		pclose(fpipe);
-    	//}
-    	//fclose(file);
-    	return EXIT_SUCCESS;
+	}
+	fclose(file);	
+	return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc != 3) {
         printf("Usage: %s <offset>\n", argv[0]);
         return 1;
     }
 
     int offset = atoi(argv[1]);
+    int inode = atoi(argv[2]);
 
     printf("**********************************************************************************************************************\n");
-    printf("Displaying Inode Information:\n");
-    int c = ils(offset);
+    printf("Displaying I-Node information:\n");
+    int c = istat(offset,inode);
     printf("**********************************************************************************************************************\n");
     printf("\n");
     return 0;
