5c003039b6d91525e67401e8f97650725b8a6b92
sonal0205 <94561078+sonal0205@users.noreply.github.com>
2023-11-14 05:43:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,116 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
     
 @app.route('/home',methods=['POST'])	
 def upload_file():
+	global fileaddress
 	uploaded_file = request.files['file']
 	if uploaded_file.filename != '':
 		upload_dir = 'Image'
 		os.makedirs(upload_dir, exist_ok=True)
 		file_address = os.path.join(upload_dir,uploaded_file.filename)
+		fileaddress=file_address
 		uploaded_file.save(file_address)
 		with open('file_address.txt','w') as file:
 			file.write(file_address)
 			return 'File uploaded successfully!'
 	else:
 		return 'No file selected'
+		
+@app.route('/set_profile',methods=['POST'])
+def set_profile():
+	global profile
+	profile = request.form['profile']
+	return 'Profile Set Successfully'
 
 @app.route('/execute', methods=['POST'])
 def execute():
     action = request.form['action']
     output = ""
 
+#VOLATILITY
+
+    if action == 'memimageinfo':
+
+        command = ["python2", "/usr/bin/vol.py", "-f", fileaddress, "imageinfo"]
+        #subprocess.run(command)
+        
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            return jsonify(error="An error occurred during img_stat execution.")
+
+    if action == 'printkey':
+        #profile = request.form['profile']
+        command = ['python2', '/usr/bin/vol.py', '-f', '/home/kali/Downloads/capstone7/Image/memdumpWin7.mem', '--profile='+profile,'hivelist']
+        #print(profile)
+        #result = subprocess.run(command, capture_output=True)
+        
+        try:
+            #output=result.stdout.decode('utf-8')
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            print(e.cmd, e.output)
+            return jsonify(error="An error occurred during printkey execution.")
+            
+    if action == 'netscan':
+        #profile = request.form['profile']
+        command = ['python2', '/usr/bin/vol.py', '-f', '/home/kali/Downloads/capstone7/Image/memdumpWin7.mem', '--profile='+profile,'netscan']
+        #print(profile)
+        #result = subprocess.run(command, capture_output=True)
+        
+        try:
+            #output=result.stdout.decode('utf-8')
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            print(e.cmd, e.output)
+            return jsonify(error="An error occurred during netscan execution.")
+            
+    if action == 'pslist':
+        #profile = request.form['profile']
+        command = ['python2', '/usr/bin/vol.py', '-f', '/home/kali/Downloads/capstone7/Image/memdumpWin7.mem', '--profile='+profile,'pslist']
+        #print(profile)
+        #result = subprocess.run(command, capture_output=True)
+        
+        try:
+            #output=result.stdout.decode('utf-8')
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            print(e.cmd, e.output)
+            return jsonify(error="An error occurred during pslist execution.")
+            
+    if action == 'psxview':
+        #profile = request.form['profile']
+        command = ['python2', '/usr/bin/vol.py', '-f', '/home/kali/Downloads/capstone7/Image/memdumpWin7.mem', '--profile='+profile,'psxview']
+        #print(profile)
+        #result = subprocess.run(command, capture_output=True)
+        
+        try:
+            #output=result.stdout.decode('utf-8')
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            print(e.cmd, e.output)
+            return jsonify(error="An error occurred during psxview execution.")
+            
+            
+#SLEUTHKIT    
     if action == 'img_stat':
         # Compile and execute img_stat tool
         command = ['gcc', 'img_stat.c', '-o', 'img_stat']
@@ -93,6 +182,96 @@ def execute():
             # Handle errors here, for example:
             return jsonify(error="An error occurred during fsstat execution.")
             
+    if action == 'ils':
+        # Get the offset from the form data
+        offset = request.form['offset']
+        try:
+            # Compile and execute fls tool
+            command = ['gcc', 'ils.c', '-o', 'ils']
+            subprocess.run(command)
+            # Replace '/path/to/your/image.dd' with the actual path to your forensic image file
+            output = subprocess.check_output(['./ils', offset], stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            #return e
+            print(type(output))
+            print(e)
+            return jsonify(error="An error occurred during ils execution.")
+            
+    if action == 'jpeg_extract':
+        # Get the offset from the form data
+        folder = request.form['folder']
+        try:
+            # Compile and execute fls tool
+            command = ['gcc', 'jpeg_extract.c', '-o', 'jpeg_extract']
+            subprocess.run(command)
+            # Replace '/path/to/your/image.dd' with the actual path to your forensic image file
+            output = subprocess.check_output(['./jpeg_extract', folder], stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            #return e
+            print(type(output))
+            print(e)
+            return jsonify(error="An error occurred during ils execution.")
+            
+    if action == 'icat':
+        # Get the offset from the form data
+        offset = request.form['offset']
+        inode = request.form['inode']
+  
+        try:
+            # Compile and execute fls tool
+            command = ['gcc', 'icat.c', '-o', 'icat']
+            subprocess.run(command)
+            # Replace '/path/to/your/image.dd' with the actual path to your forensic image file
+            output = subprocess.check_output(['./icat', offset, inode], stderr=subprocess.STDOUT, text=True)
+            
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            return jsonify(error="An error occurred during icat execution.")
+            
+    if action == 'istat':
+        # Get the offset from the form data
+        offset = request.form['offset']
+        inode = request.form['inode']
+  
+        try:
+            # Compile and execute fls tool
+            command = ['gcc', 'istat.c', '-o', 'istat']
+            subprocess.run(command)
+            # Replace '/path/to/your/image.dd' with the actual path to your forensic image file
+            output = subprocess.check_output(['./istat', offset, inode], stderr=subprocess.STDOUT, text=True)
+            
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            return jsonify(error="An error occurred during istat execution.")
+
+    if action == 'sorter':
+        # Get the offset from the form data
+        offset = request.form['offset']
+        try:
+            # Compile and execute fls tool
+            command = ['gcc', 'sorter.c', '-o', 'sorter']
+            subprocess.run(command)
+            # Replace '/path/to/your/image.dd' with the actual path to your forensic image file
+            output = subprocess.check_output(['./sorter', offset], stderr=subprocess.STDOUT, text=True)
+            # Wrap the output in <pre> tags and replace newlines with <br> tags
+            output = '<pre>' + output.replace('\n', '<br>') + '</pre>'
+        except subprocess.CalledProcessError as e:
+            # Handle errors here, for example:
+            #return e
+            print(type(output))
+            print(e)
+            return jsonify(error="An error occurred during fls execution.")
+    
     if action == 'stringsearch':
         searchString = request.form['searchString']
         offset = request.form['offset']
@@ -144,7 +323,6 @@ def execute():
         except subprocess.CalledProcessError as e:
             # Handle errors here, for example:
             return jsonify(error="An error occurred during srch_strings execution.")
-           
 
     print(type(output))
     return output
